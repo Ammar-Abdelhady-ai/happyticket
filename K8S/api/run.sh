a3686ac4c77a030f306c006a37cad2faa4181b74
Ammar Abdelhady <ammarabdelhady8@gmail.com>
2025-07-03 02:33:52 +0300

    edit api files

--- K8S/api/run.sh
+++ K8S/api/run.sh
@@ -1,9 +1,10 @@
 kubectl apply -f wwwroot-pv.yml
 kubectl apply -f wwwroot-pvc.yml
 
+kubectl apply -f api-appsettings.yml
 kubectl apply -f api-config.yml
 kubectl apply -f api-secret.yml
-kubectl apply -f api-appsettings.yml
+
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
 kubectl apply -f hpa.yml
